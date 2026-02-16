0b0bebcad89a11426f737edece7f61c966a931b2
audstudios <billy.e.riley@gmail.com>
2026-02-16 18:25:26 -0500

    Next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@napi-rs/canvas'],
+  experimental: {
+    serverComponentsExternalPackages: ['@napi-rs/canvas'],
+  },
   webpack(config, { isServer }) {
+    // Handle .node files differently for client vs server
     config.module.rules.push({
       test: /\.node$/,
       use: isServer ? 'node-loader' : 'null-loader',
     })
+    
     return config
   },
 }
